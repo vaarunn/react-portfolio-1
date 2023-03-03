2012d984c79872cb8509f2de47d58fbf914d55ce
sud0x00 <sudhanva0asvl@outlook.com>
2023-03-03 21:53:46 +0530

    modified:   src/App.js 	modified:   src/components/LandingPage.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import { BrowserRouter as Routes , Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -7,18 +7,14 @@ import Blog from './pages/Blog';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
+    <div className="app">   
         <Routes>
           <Route path="/about" element={<About/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/blog" element={<Blog/>} />
-          <Route path="/">
-            <LandingPage />
-          </Route>
+          <Route path="/" element={<LandingPage/>} />
         </Routes>
-      </div>
-    </Router>
+    </div>
   );
 };
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
+
 
 const LandingPage = () => {
   return (
