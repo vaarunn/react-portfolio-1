1353c9d5df5df4625537090114cbdf3fa6c75763
sud0x00 <sudhanva0asvl@outlook.com>
2023-03-03 22:03:38 +0530

    modified:   src/App.js 	modified:   src/components/LandingPage.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routes , Route } from 'react-router-dom';
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -7,14 +7,16 @@ import Blog from './pages/Blog';
 
 const App = () => {
   return (
-    <div className="app">   
-        <Routes>
-          <Route path="/about" element={<About/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/blog" element={<Blog/>} />
-          <Route path="/" element={<LandingPage/>} />
-        </Routes>
-    </div>
+    <Router>
+      <div className="app">   
+          <Routes>
+            <Route path="/about" element={<About/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/blog" element={<Blog/>} />
+            <Route path="/" element={<LandingPage/>} />
+          </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
-
 const LandingPage = () => {
   return (
     <div className="landing-page">
